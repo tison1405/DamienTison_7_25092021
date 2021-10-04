ff841519962ef663314705016ce5394a94ac5ba0
tison1405 <77247038+tison1405@users.noreply.github.com>
2021-10-04 21:14:08 +0200

    lancement router

--- frontend/commania/src/App.vue
+++ frontend/commania/src/App.vue
@@ -1,46 +1,32 @@
-<script>
-import Herolink from './components/HeroLink.vue';
-import LoginImages from './components/logincard.vue';
-import signImage from './components/SignLink.vue'
-
-export default {
-	name: 'App',
-	components: {
-		Herolink,
-    LoginImages,
-    signImage
-	}
-}
-</script>
-
 <template>
-	<div class="container" id="app">
-   <div class="row">
-      <div class="col">
-         <h1>Commania</h1>
-      </div>
-   </div>
-   <Herolink/>
-   <div class="row">
-     <div class="col-12 col-lg-6 card">
-        <LoginImages url="/" text="Inscrivez vous"/>
-        </div>
-      
-     <div class="col-12 col-lg-6 card">
-        <signImage url="/" text="Identifiez vous"/>
-     </div>
-   </div> 
-
-   <div class="row">
-
-   </div>
-</div>
-
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+#nav {
+  padding: 30px;
 
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
