0538a12122f0169d297e70824f60239a79676c29
tison1405 <77247038+tison1405@users.noreply.github.com>
2021-10-04 20:51:21 +0200

    debut front vue

---
+++ frontend/commania/src/components/SignLink.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name:'signImage',
+    props:['url','text']   
+}
+</script>
+
+<template>
+    <div class="card-body2">
+          <a :href="url">{{ text }}</a>
+        </div>
+</template>
+
+<style>
+.card-body2{
+  background-image: url("../assets/images/identifier.jpg");
+	height: 160px;
+	background-repeat: no-repeat;
+	background-position-x: center;
+  text-align: end;
+}
+a{
+    text-decoration: none;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    flex-direction: column;
+    text-align: end;
+}
+</style>
---
+++ frontend/commania/src/components/logincard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+	name: 'LoginImages',
+    props:['url','text']
+}
+</script>
+
+<template>
+    <div class="card-body1">
+        <a :href="url">{{ text }}</a>
+    </div>
+</template>
+
+<style>
+.card-body1{
+    background-image: url("../assets/images/signing.jpg");
+	height: 160px;
+	background-repeat: no-repeat;
+	background-position-x: center;
+    text-align: end;
+}
+  a{
+    text-decoration: none;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    flex-direction: column;
+    text-align: end;
+}
+</style>
